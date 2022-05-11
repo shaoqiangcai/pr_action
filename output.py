edac12e41d91bd89f022c12187c8ebf7e3aeb10d
fish0607 <yuj0607@163.com>
2022-05-11 15:18:31 +0800

    Update output.py

--- output.py
+++ output.py
@@ -3,5 +3,5 @@ result = {
   "task":"OK",
   "msg": "wahah"
 }
-print(f"::set-output name=Task::{result['task']}")
-print(f"::set-output name=MSG::{result['msg']}")
+print(f"::set-output name={Task}::{result['task']}")
+print(f"::set-output name={MSG}::{result['msg']}")
